dbcfb7866749599428ace233bf254d217c9209ba
Uğur Önder Karapunar <142834627+ugurronderkarapunar@users.noreply.github.com>
2026-05-15 11:20:03 +0300

    riot_api_client.py

--- riot_api_client.py
+++ riot_api_client.py
@@ -5,6 +5,18 @@ import streamlit as st
 def get_champion_square(champion_name):
     """Riot Data Dragon'dan şampiyon portresini döndürür."""
     name_formatted = champion_name.replace(" ", "").replace("'", "").lower()
+    # Bazı özel durumlar
+    special_names = {
+        "kai'sa": "kaisa",
+        "khazix": "khazix",
+        "velkoz": "velkoz",
+        "jarvaniv": "jarvaniv",
+        "missfortune": "missfortune",
+        "twistedfate": "twistedfate",
+        "masteryi": "masteryi",
+        "leesin": "leesin",
+    }
+    name_formatted = special_names.get(name_formatted, name_formatted)
     url = f"https://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/{name_formatted.capitalize()}.png"
     try:
         response = requests.get(url)
@@ -12,5 +24,4 @@ def get_champion_square(champion_name):
             return url
     except:
         pass
-    # Yedek görsel
     return "https://ddragon.leagueoflegends.com/cdn/13.18.1/img/champion/Default.png"
